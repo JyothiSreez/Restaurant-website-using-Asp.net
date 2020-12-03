5c336746af904ede553f8b7ae71477cf313078ad
JyothiSreez <38304110+JyothiSreez@users.noreply.github.com>
2020-12-03 15:26:39 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class dbdisconnect
+{
+    SqlConnection con = new SqlConnection("server= .; uid=sa; pwd=admin123; database = newest-db");
+    SqlCommand cmd;
+    SqlDataAdapter adp;
+    DataSet ds = new DataSet();
+
+    public DataSet exdisc(string q)
+    {
+      try{
+           adp = new SqlDataAdapter(q,con);
+           adp.Fill(ds);
+           return ds;
+      }
+        catch(Exception ex)
+      {
+        return ds;
+
+        }
+    }
+}
